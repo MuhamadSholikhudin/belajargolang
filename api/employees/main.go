5fda80f3af30f95c6ee5f6424880ed8c767adc45
MuhamadSholikhudin <muhammadsholihudin18@gmail.com>
2022-09-22 16:07:54 +0700

    2022 09 22 16:00

---
+++ api/employees/main.go
@@ -0,0 +1,436 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Employee struct {
+	//penamaan Camel Cas untuk Import Package supaya bisa di pakai dari luar
+	Number_of_employees string `json:"number_of_employees"` // `` cara membuat penamaan ulang pada golang pada saat di GET
+	National_id         string `json:"national_id"`
+}
+
+type Dateemployee struct {
+	Hire_date string `json:"hire_date"`
+	Date_out  string `json:"date_out"`
+}
+
+type Count struct {
+	sum int
+}
+
+type Job struct {
+	Job_level string `json:"job_level"`
+}
+
+type Department struct {
+	Department string `json:"department"`
+}
+
+func Conn() (*sql.DB, error) {
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/hrdit")
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
+
+var datas []Employee
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	db, err := Conn()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select number_of_employees, national_id from employees where id > ?", 0)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer rows.Close()
+
+	var result []Employee
+
+	for rows.Next() {
+		var each = Employee{}
+		var err = rows.Scan(&each.Number_of_employees, &each.National_id)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		result = append(result, each)
+	}
+
+	resp, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Write([]byte(resp))
+
+}
+
+func Get(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
+	nik, _ := strconv.Atoi(vars["number_of_employees"])
+	ktp, _ := strconv.Atoi(vars["national_id"])
+
+	var db, err = Conn()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	var result = Employee{}
+	number_of_employees := nik
+	national_id := ktp
+	err = db.
+		QueryRow("select number_of_employees, national_id from employees where number_of_employees = ? AND national_id = ?", number_of_employees, national_id).
+		Scan(&result.Number_of_employees, &result.National_id)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	resp, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Write([]byte(resp))
+}
+
+func GetKaryawan(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
+	nik, _ := strconv.Atoi(vars["number_of_employees"])
+	ktp, _ := strconv.Atoi(vars["national_id"])
+
+	var db, err = Conn()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	var result = Count{}
+
+	number_of_employees := nik
+	national_id := ktp
+
+	err = db.
+		QueryRow("select count(id) as sum from employees where number_of_employees = ? AND national_id = ?", number_of_employees, national_id).
+		Scan(&result.sum)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	var statushttp string
+
+	if result.sum > 0 {
+		statushttp = "200"
+	} else {
+		statushttp = "400"
+	}
+
+	response := map[string]interface{}{
+		"status": statushttp,
+	}
+
+	resp, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Write([]byte(resp))
+}
+
+func GetResign(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
+	nik, _ := strconv.Atoi(vars["number_of_employees"])
+	ktp, _ := strconv.Atoi(vars["national_id"])
+
+	var db, err = Conn()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	var result = Dateemployee{}
+
+	number_of_employees := nik
+	national_id := ktp
+
+	err = db.
+		QueryRow("select hire_date, COALESCE(date_out, '0000-00-00') as date_out from employees where number_of_employees = ? AND national_id = ?", number_of_employees, national_id).
+		Scan(&result.Hire_date, &result.Date_out)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	resp, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Write([]byte(resp))
+}
+
+func GetJobs(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	db, err := Conn()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select job_level from jobs where level > ? and job_level != ? order by level desc ", 8, "NONE")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer rows.Close()
+
+	var result []Job
+
+	for rows.Next() {
+		var each = Job{}
+		var err = rows.Scan(&each.Job_level)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		result = append(result, each)
+	}
+
+	resp, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Write([]byte(resp))
+}
+
+func GetDepartments(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	db, err := Conn()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT department FROM departments WHERE department != 'HR GA SEA' and 	department != 'PLANT E' and 	department != 'MATL PUR' and 	department != 'CHANGE' and 	department != 'PLANT C' and 	department != 'PLANT H' and 	department != 'PLANT D' and 	department != 'PLANT E' and 	department != 'BUSINESS' and 	department != 'PLANT A AND B' and 	department != 'CHEMICAL ADVISOR' and department != 'EXECUTIVE SR. DIR HWI 2' and department != 'NONE' order by department asc")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer rows.Close()
+
+	var result []Department
+
+	for rows.Next() {
+		var each = Department{}
+		var err = rows.Scan(&each.Department)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		result = append(result, each)
+	}
+
+	resp, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Write([]byte(resp))
+}
+
+func GetGedungs(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	response := []string{
+		"GEDUNG A", "GEDUNG B", "GEDUNG C", "GEDUNG D", "GEDUNG E", "GEDUNG F", "GEDUNG G", "GEDUNG H", "LAMINATING", "GUDANG SETTING", "WAREHOUSE (MATERIAL)", "SABLON", "EMBOSS", "TRAINING CENTER", "MAIN OFFICE", "EPTE (BEACUKAI)", "POS SECURITY", "KANTOR SERIKAT", "MESS / DRIVER",
+	}
+
+	resp, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Write([]byte(resp))
+
+}
+
+func Post(w http.ResponseWriter, r *http.Request) {
+	//untuk membuat json pertama kita harus set Header
+	w.Header().Set("Content-Type", "application/json")
+
+	// data := r.Body
+	//mendecode requset body langsung menjadi json
+	data := Employee{}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	fmt.Println(data)
+
+	db, err := Conn()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	var result = Employee{}
+
+	err = db.
+		QueryRow("select number_of_employees, national_id FROM employees where number_of_employees = ? and national_id = ?", data.Number_of_employees, data.National_id).
+		Scan(&result.Number_of_employees, &result.National_id)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	resp, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Write([]byte(resp))
+
+}
+
+// func Update(w http.ResponseWriter, r *http.Request) {
+// 	//untuk membuat json pertama kita harus set Header
+// 	w.Header().Set("Content-Type", "application/json")
+
+// 	//mendecode requset body langsung menjadi json
+// 	data := Employee{}
+// 	err := json.NewDecoder(r.Body).Decode(&data)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 	}
+
+// 	db, err := Conn()
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		return
+// 	}
+// 	defer db.Close()
+
+// 	_, err = db.Exec("update employees set name = ? where id = ?", data.Name, data.Id)
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		return
+// 	}
+// 	fmt.Println("insert success!")
+
+// 	response := map[string]interface{}{
+// 		"status": "Oke",
+// 	}
+// 	err = json.NewEncoder(w).Encode(response)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+
+// }
+
+// func Delete(w http.ResponseWriter, r *http.Request) {
+// 	//untuk membuat json pertama kita harus set Header
+// 	w.Header().Set("Content-Type", "application/json")
+
+// 	//mendecode requset body langsung menjadi json
+// 	data := Employee{}
+// 	err := json.NewDecoder(r.Body).Decode(&data)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 	}
+
+// 	db, err := Conn()
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		return
+// 	}
+// 	defer db.Close()
+
+// 	_, err = db.Exec("delete from user where id = ?", data.Id)
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		return
+// 	}
+// 	fmt.Println("Delete success!")
+
+// 	response := map[string]interface{}{
+// 		"status": "Oke",
+// 	}
+// 	err = json.NewEncoder(w).Encode(response)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+
+// }
+
+func main() {
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/employees/{number_of_employees}", Get).Methods("GET")
+	r.HandleFunc("/resign/{number_of_employees}/{national_id}", GetKaryawan).Methods("GET")
+	r.HandleFunc("/resigndate/{number_of_employees}/{national_id}", GetResign).Methods("GET")
+	r.HandleFunc("/resignjobs", GetJobs).Methods("GET")
+	r.HandleFunc("/resigndepartments", GetDepartments).Methods("GET")
+	r.HandleFunc("/resignbuildings", GetGedungs).Methods("GET")
+
+	// r.HandleFunc("/user/{id}", Update).Methods("PUT")
+	r.HandleFunc("/resign", Post).Methods("POST")
+	// r.HandleFunc("/user/{id}", Delete).Methods("DELETE")
+
+	fmt.Println("LIsten on Port 127.0.0.1:8880")
+	http.ListenAndServe(":8880", r)
+
+}
